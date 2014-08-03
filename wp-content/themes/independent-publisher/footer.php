4707a35c3788568448bef113ff383361478a865d
Shannon Smith <shannones2013@gmail.com>
2014-08-02 20:18:06 -0700

    update

--- wp-content/themes/independent-publisher/footer.php
+++ wp-content/themes/independent-publisher/footer.php
@@ -18,7 +18,7 @@
 <li><a href="https://twitter.com/SFdevgirl" target="_blank"><img src="http://i1144.photobucket.com/albums/o489/shannones1104/Twitter_zpsddc0f426.png"/></a></li>
 <li><a href="https://github.com/SFdevgirl" target="_blank"><img src="http://i1144.photobucket.com/albums/o489/shannones1104/Github_zps90a82b60.png"/></a></li>
 <li><a href="https://www.linkedin.com/profile/view?id=309027165&trk=spm_pic?id=309027165&trk=spm_pic" target="_blank"><img src="http://i1144.photobucket.com/albums/o489/shannones1104/Linkedin_zps82e4ab7d.png"/></a></li>
-<li><a href="http://shannonlearnscode.tumblr.com" target="_blank"><img src="http://i1144.photobucket.com/albums/o489/shannones1104/Tumblr_zps0e446b39.png"/></a></li>
+
 <li><a href="mailto:shannon@shannonknowscode.com"><img src="http://i1144.photobucket.com/albums/o489/shannones1104/Email_zpsf34922f3.png"/></a></li>
 </ul>
 <span class="copy">&copy;opyright 2014. <a href="/contact.html">Shannon Smith</a> </span> 
