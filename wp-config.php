41646079e37bae241579fc34cf34b4657b9532ef
Shannon Smith <shannones2013@gmail.com>
2014-08-09 03:00:56 -0700

    config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'localWP');
+define('DB_NAME', 'shannonknowscode_com');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'shasmi90');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Dirtyrich1');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.shannonknowscode.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
