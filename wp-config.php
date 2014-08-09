e1ce259d7efb2ed554691b6115ab2ec06c4cecff
Shannon Smith <shannones2013@gmail.com>
2014-08-09 02:48:45 -0700

    gitignore themes

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'shannonknowscode_com');
+define('DB_NAME', 'localWP');
 
 /** MySQL database username */
-define('DB_USER', 'shasmi90');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Dirtyrich1');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.shannonknowscode.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
